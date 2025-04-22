704c5175df51d26887a5203cb92c4d766f3be74e
Zelchi <tininhakawaii@gmail.com>
2025-04-22 19:29:13 -0300

    - Implementado roteamento no AppAdmin com um link para o Dashboard.
    - Atualizado o AppUser para redirecionar para Home e adicionado roteamento para Home, Agendamentos e Produtos.
    
    - Modificado o componente Header para refletir a rota atual.

--- src/admin/AppAdmin.tsx
+++ src/admin/AppAdmin.tsx
@@ -1,17 +1,35 @@
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: start;
+    overflow-x: hidden;
+    overflow-y: auto;
+    background-color: #1c1c1c;
+    color: #ffffff;
+`
+
 interface AppAdminProps {
     setApp: (app: boolean) => void;
 }
 
-export const AppAdmin = ({setApp}: AppAdminProps) => {
+export const AppAdmin = ({ setApp }: AppAdminProps) => {
+    const navigate = useNavigate();
+
     return (
-        <>
-            <div>
-                <h1>Admin</h1>
-                <button onClick={() => setApp(true)}>Voltar</button>
-            </div>
-            <div>
-                <h1>Teste</h1>
-            </div>
-        </>
+        <Container>
+            <h1>Admin</h1>
+            <button onClick={() => { setApp(true); navigate("/home"); }}>Voltar</button>
+            <Routes>
+                <Route path="*" element={<Navigate to={"/dashboard"} />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Container>
     )
 }
--- src/user/AppUser.tsx
+++ src/user/AppUser.tsx
@@ -1,6 +1,6 @@
 import { Header } from "./components/header/header"
 import { BannerProvider } from "./context/BannerProvider"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { Home } from "./pages/home/home"
 import { Agendamentos } from "./pages/agendamento/agendamentos"
 import { Produtos } from "./pages/produtos/produtos"
@@ -12,8 +12,9 @@ export const AppUser = () => {
             <Header />
             <BannerProvider>
                 <Routes>
-                    <Route path="*" element={<Home />} />
-                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<Navigate to="/home" />} />
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    <Route path="/home" element={<Home />} />
                     <Route path="/agendamentos" element={<Agendamentos />} />
                     <Route path="/produtos" element={<Produtos />} />
                     <Route path="/cursos" element={<Cursos />} />
