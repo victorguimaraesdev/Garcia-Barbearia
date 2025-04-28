605731793181668d764325db3a8181c51d3a80e3
Zelchi <tininhakawaii@gmail.com>
2025-04-28 02:28:22 -0300

    classe API na GoogleAuth

--- src/user/components/GoogleAuth.tsx
+++ src/user/components/GoogleAuth.tsx
@@ -1,5 +1,6 @@
 import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
 import { useState, useEffect } from "react";
+import API from "../../utils/API";
 import axios from "axios";
 
 interface Props {
@@ -12,13 +13,13 @@ export const Login = ({ setAutorizado }: Props) => {
     const EnviarCredenciais = async ({ credential, clientId }: CredentialResponse) => {
         if (!credential || !clientId) return;
 
-        const { status, data } = await axios.post("http://localhost:3000/api/usuarios", { credential, clientId, role: "2" });
+        const { status, data } = await API.POST("/usuarios", { credential, clientId, role: "2" });
         if (status === 200) {
             const token = data.authorization;
             localStorage.setItem("token", token);
             axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
             setAutorizado(true);
-            return; 
+            return;
         }
         if (status === 401) {
             localStorage.setItem("token", "");
